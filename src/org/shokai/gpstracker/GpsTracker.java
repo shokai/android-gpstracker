08395f9bc04a88c3fe95ab4430c4494217a22e96
Sho Hashimoto <hashimoto@shokai.org>
2010-05-28 16:13:59 +0900

    onLocationChanged

--- src/org/shokai/gpstracker/GpsTracker.java
+++ src/org/shokai/gpstracker/GpsTracker.java
@@ -2,7 +2,9 @@ package org.shokai.gpstracker;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,7 @@ import android.view.Menu;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class GpsTracker extends Activity implements OnClickListener{
+public class GpsTracker extends Activity implements OnClickListener, LocationListener{
     /** Called when the activity is first created. */
 	
 	private Button buttonPostLocation;
@@ -19,7 +21,8 @@ public class GpsTracker extends Activity implements OnClickListener{
 
     private final int MENU_ZOOM_IN = 1;
     private final int MENU_ZOOM_OUT = 2;
-    private final int MENU_SAVE_LOCATION = 3;
+    private final int MENU_UPDATE_LOCATION = 3;
+    private final int MENU_SHOW_LAST_LOCATION = 4;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,15 +34,20 @@ public class GpsTracker extends Activity implements OnClickListener{
     }
 
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
 		switch(v.getId()){
 		case R.id.buttonPostLocation:
-			LocationManager locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-			Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            
-            double lat = location.getLatitude() * 1E6;
-            double lon = location.getLongitude() * 1E6;
-			message(Double.toString(lat)+", "+Double.toString(lon));
+			LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+			lm.removeUpdates(this);
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 100, this);
+			
+			Criteria crit = new Criteria();
+			crit.setAccuracy(Criteria.ACCURACY_FINE);
+			String provider = lm.getBestProvider(crit, true);
+			Location loc = lm.getLastKnownLocation(provider);
+
+	        double lat = loc.getLatitude();
+	        double lon = loc.getLongitude();
+			message("start: "+Double.toString(lat)+", "+Double.toString(lon));
 			break;
 		}
 	}
@@ -49,11 +57,31 @@ public class GpsTracker extends Activity implements OnClickListener{
       boolean supRetVal = super.onCreateOptionsMenu(menu);
       menu.add(0, this.MENU_ZOOM_IN, 0, "zoom in");
       menu.add(0, this.MENU_ZOOM_OUT, 1, "zoom out");
-      menu.add(0, this.MENU_SAVE_LOCATION, 2, "save");
+      menu.add(0, this.MENU_UPDATE_LOCATION, 2, "save");
+      menu.add(0, this.MENU_SHOW_LAST_LOCATION, 3, "show last location");
       return supRetVal;
     }
 	
 	public void message(String mes){
 		this.textViewMessage.setText(mes + "\r\n" + this.textViewMessage.getText());
+	}
+
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+		message("update: "+Double.toString(lat)+", "+Double.toString(lon));
+	}
+
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
 	}
 }
