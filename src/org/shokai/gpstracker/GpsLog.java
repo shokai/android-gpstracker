38b291882ec56aa71c4f78e126cef89d9b95e199
Sho Hashimoto <hashimoto@shokai.org>
2010-06-02 11:00:56 +0900

    restore GpsLog from SD Card

--- src/org/shokai/gpstracker/GpsLog.java
+++ src/org/shokai/gpstracker/GpsLog.java
@@ -26,7 +26,13 @@ public class GpsLog {
         }
         else{
             context.trace("SD Card not Found");
-            new AlertDialog.Builder(context).setMessage("SDカードが必要です").setPositiveButton("OK", null).show();
+            new AlertDialog.Builder(context).setMessage("this app uses SD Card").setPositiveButton("OK", null).show();
+        }
+        try{
+            this.loadLog(); 
+        }
+        catch(Exception e){
+            Log.e("GpsTracker", "log load error");
         }
     }
     
@@ -76,6 +82,50 @@ public class GpsLog {
             throw e;
         }
         context.trace("saveLog : " + f.getPath());
+    }
+    
+    private boolean loadLog() throws Exception{
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        try{
+            return this.loadLog(year, month, day);
+        }
+        catch(Exception e){
+            throw e;
+        }
+    }
+    
+    private boolean loadLog(int year, int month, int day) throws Exception{
+        File f = new File(this.dataDir, year+"-"+month+"-"+day+".txt");
+        if(!f.exists()) return false;
+        context.trace("load Log - " + f.getPath());
+        try{
+            InputStream is = new FileInputStream(f);
+            byte[] bytes = new byte[is.available()];
+            is.read(bytes);
+            is.close();
+            String[] lines = new String(bytes).split("\n");
+            List<GeoPoint> points = new ArrayList<GeoPoint>();
+            for(String line : lines){
+                String[] items = line.split("[    ]*,[    ]*");
+                if(items.length > 1){
+                    double lat = Double.parseDouble(items[0]);
+                    double lon = Double.parseDouble(items[1]);
+                    context.trace("load Log - "+lat + ", " + lon);
+                    points.add( new GeoPoint((int) (lat * 1E6), (int) (lon * 1E6)) );
+                }
+            }
+            if (points.size() > 0){
+                this.points = points;
+                return true;
+            }
+            return false;
+        }
+        catch(Exception e){
+            throw e;
+        }
     }
     
 }
